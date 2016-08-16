508931fa762d99a8c774047fc13fa1d06940e1ce
dakarl <dakarl@asu.edu>
2016-08-16 08:20:13 -0500

    removed old comments

--- lib.js
+++ lib.js
@@ -30,8 +30,6 @@ function logUnderlingNames (manager, employees) {
     })
 }
 
-
-
 function groupByManagerName (employees) {
   const boss = {}
   employees
@@ -40,61 +38,6 @@ function groupByManagerName (employees) {
 
   console.log(boss)
 }
-
-//
-//
-// function higherOrderUnderlings(manager,employees){
-//   function hasManager (employee) {
-//     return employee.manager === manager
-//   }
-//   return employees.filter(hasManager)
-// }
-//
-//
-// function higherOrderNames(employees){
-//   function getName(employee){
-//     return employee.name
-//   }
-// return employees.map(getName)
-//   }
-//
-// function higherOrderLog (names) {
-//     function logName (name) {
-//       console.log(name)
-//     }
-//     names.forEach(logName)
-// }
-//
-//
-//  //filtering
-// function underlings (manager, employees) {
-//   const returnArray = []
-//
-//   function hasManager(employee){
-//     return employee.manager===manager
-//   }
-//
-//   for (let i = 0; i < employees.length; ++i) {
-//     if (hasManager(employees[i])) {
-//       returnArray.push(employees[i])
-//     }
-//   }
-//   return returnArray
-// }
-// //mapping / transforming
-// function names(employees){
-//   const result = []
-//   for (let employee of employees){
-//     result.push(employee.name)
-//   }
-//   return result
-// }
-// //foreach
-// function log (names) {
-//   for (let name of names) {
-//     console.log(name)
-//   }
-// }
 
 module.exports = {
   employee,
